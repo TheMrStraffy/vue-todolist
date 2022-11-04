3039a695badc8484a350acba24ad2ddc4fe2072c
TheMrStraffy <carmelo.miuccio2@gmail.com>
2022-11-04 15:49:38 +0100

    [added]removeTask

--- js/script.js
+++ js/script.js
@@ -3,11 +3,36 @@ const {createApp} = Vue;
 createApp({
   data(){
     return{
-      logo: "img/logo.png"
+      logo: "img/logo.png",
+      typeTask: "",
+      error: "",
+      tasks: [
+        {
+          text: "Studiare assai",
+          done: false
+        },
+        {
+          text: "Comporre almeno una canzone",
+          done: true
+        },
+        {
+          text: "Scrivere 3 poesie",
+          done: false
+        },
+      ],
+      createTask: {text: this.typeTask, done: false}
+
     }
   },
   methods:{
-
+    newTask(){
+      this.tasks.push(this.createTask);
+    },
+    removeTask(index){
+      if(this.tasks[index].done){
+        this.tasks.splice(index,1)
+      }
+    }
   },
   mounted(){
 
