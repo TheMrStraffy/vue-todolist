3228293ce8a2aa00d9510acab7993cde5b9b9b5e
TheMrStraffy <carmelo.miuccio2@gmail.com>
2022-11-04 12:50:01 +0100

    [init]

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const {createApp} = Vue;
+
+createApp({
+  data(){
+    return{
+      logo: "img/logo.png"
+    }
+  },
+  methods:{
+
+  },
+  mounted(){
+
+  },
+  created(){
+
+  },
+}).mount('#app')
